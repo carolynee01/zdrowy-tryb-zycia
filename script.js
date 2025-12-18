60a7c7eb608dab63b7a9602852dda1978f047451
Karolina Sawicka <138665979+carolynee01@users.noreply.github.com>
2025-12-18 13:22:05 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Kod uruchomi się po załadowaniu drzewa DOM
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Strona „Zdrowy styl życia” załadowana.');
+
+    // TODO: tutaj dodamy:
+    // 1. Slider z poradami
+    // 2. Kalkulator BMI
+    // 3. Walidację formularza kontaktowego
+});
